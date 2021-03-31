0c152ae0762c735176214946f87778af5232e50e
JoeyStrandnes <joey.strandnes@hotmail.com>
2021-03-31 19:46:49 +0200

    Testing and updated gitignore

--- Lab_1/main.c
+++ Lab_1/main.c
@@ -1,1 +1,43 @@
 //LAB 1
+#include <stdio.h>
+
+
+int main(int argc, char** argv){
+
+  int counter = 0;
+/*
+  int temp[] = {69,2,3,4};
+  int* ptr = temp;
+
+  printf("%li \n", sizeof(*(ptr)));
+*/
+  if (argc > 20) {
+    printf("%s\n", "Matat in för många");
+  }
+
+  else{
+    //printf("%li\n", sizeof(*argv[1]));
+
+    for (int i = 0; i < argc; i++) {
+      char* ptr = argv[i];
+      printf("%s",ptr);
+      printf(" %p",ptr);
+      printf(" %p",argv[i]);
+      printf(" %li", sizeof(*(ptr)));
+      printf(" %li \n", sizeof(*(argv[i])));
+      for(unsigned int k = 0; k < sizeof(*ptr); k++){
+        printf("%d ", k);
+        printf("%c", (argv[i][k]));
+
+        counter++;
+      }
+
+      printf("    %d\n", counter);
+      counter = 0;
+    }
+  }
+
+
+  return 0;
+
+}
